d26960665f916ade0c2fda285d1c2152ff34fcc1
madhavi khunt <127251335+madhavi-khunt@users.noreply.github.com>
2024-04-11 11:31:24 +0530

    Add files via upload

---
+++ Add to cart/src/Data.js
@@ -0,0 +1,50 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Plain",
+    price: 45,
+    img: "images/1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Paper Roast",
+    price: 65,
+    img: "images/2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Onion Roast",
+    price: 80,
+    img: "images/3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Egg Parotta",
+    price: 55,
+    img: "images/4.jpg",
+  },
+  {
+    id: "thiecbawdjksadjk",
+    name: "Plain Omelette",
+    price: 25,
+    img: "images/5.jpg",
+  },
+  {
+    id: "iuertrywebncdjksadjk",
+    name: "Kadai Fry",
+    price: 120,
+    img: "images/6.jpg",
+  },
+  {
+    id: "thierytbvcbvzdhadjk",
+    name: "Egg Veechu",
+    price: 65,
+    img: "images/7.jpg",
+  },
+  {
+    id: "trfoiwfcnbcawdjksadjk",
+    name: "Egg Roast",
+    price: 45,
+    img: "images/8.jpg",
+  },
+];
